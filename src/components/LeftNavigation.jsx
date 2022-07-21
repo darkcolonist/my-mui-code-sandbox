5a6213235a16686485e0e6f53e2c28b5a339f4e4
Christian Noel Reyes <darkcolonist@gmail.com>
2022-07-21 17:03:45 +0800

    verbiage

--- src/components/LeftNavigation.jsx
+++ src/components/LeftNavigation.jsx
@@ -1,6 +1,7 @@
 import { ListItemIcon, MenuItem, MenuList } from "@mui/material";
 import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
+import AnimationIcon from "@mui/icons-material/Animation";
 
 export default function () {
   return (
@@ -13,7 +14,7 @@ export default function () {
       </MenuItem>
       <MenuItem component={Link} to="transitions">
         <ListItemIcon>
-          <HomeIcon />
+          <AnimationIcon />
         </ListItemIcon>
         Transitions
       </MenuItem>
